830823df2e7a5d2d005b9b9c4eef9af903d38f5e
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-03-27 08:10:32 -0300

    feat: creating design for redirect page

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import styles from "../styles/Raiz.module.css";
 
 export default function Home() {
     const router = useRouter();
@@ -11,6 +12,9 @@ export default function Home() {
     } , [router]);
 
     return (
-        <p>Redirecting …</p>
+        <div className={styles.container}>
+            <div className={styles.spinner}></div>
+            <h3>Redirecting…</h3>
+        </div>
     );
 }
